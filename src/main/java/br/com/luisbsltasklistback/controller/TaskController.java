482738d9e4c4bf2e38c539c7c3ed2cc756ede8ec
Luis Ismael Lima <luisbsl@gmail.com>
2020-01-07 23:23:58 -0300

    primeiro commit

---
+++ src/main/java/br/com/luisbsltasklistback/controller/TaskController.java
@@ -0,0 +1,16 @@
+package br.com.luisbsltasklistback.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tasks")
+public class TaskController {
+	
+	@GetMapping
+	public String getTasks() {
+		return "Tasks List";
+	}
+
+}
